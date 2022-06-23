0048c3b0213c99a82e3cd2f8910626490ea9728b
Sean Knox <sean@seanknox.net>
2022-06-23 13:23:27 +0100

    Enabled nullable region and returned the object from the popup in related methods

---
+++ src/Maui.Plugins.PageResolver/PopupExtensions.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Maui.Views;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui.Controls;
+using System.Threading.Tasks;
+
+namespace Maui.Plugins.PageResolver
+{
+    public static class PopupExtensions
+    {
+#nullable enable
+        public static async Task<object?> ShowPopupAsync<T>(this Page page) where T : Popup
+        {
+            var resolvedPopup = Resolver.Resolve<T>();
+            return await page.ShowPopupAsync(resolvedPopup);
+        }
+        public static async Task<object?> ShowPopupAsync<T>(this Page page, params object[] parameters) where T : Popup
+        {
+            var resolvedPopup = ActivatorUtilities.CreateInstance<T>(Resolver.GetServiceProvider(), parameters);
+            return await page.ShowPopupAsync(resolvedPopup);
+        }
+#nullable disable
+        public static void ShowPopup<T>(this Page page) where T : Popup
+        {
+            var resolvedPopup = Resolver.Resolve<T>();
+            page.ShowPopup(resolvedPopup);
+        }
+
+        public static void ShowPopup<T>(this Page page, params object[] parameters) where T : Popup
+        {
+            var resolvedPopup = ActivatorUtilities.CreateInstance<T>(Resolver.GetServiceProvider(), parameters);
+            page.ShowPopup(resolvedPopup);
+        }
+    }
+}
